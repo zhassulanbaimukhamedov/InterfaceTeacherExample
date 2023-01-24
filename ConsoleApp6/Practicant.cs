6d722ca2feec9563c3a0d8ba60510489a9693b0c
user <user@WIN-QPM356H66DK>
2023-01-24 22:53:09 +0600

    commits

---
+++ ConsoleApp6/Practicant.cs
@@ -0,0 +1,13 @@
+﻿
+public class Practicant
+{
+    public string Name { get; set; }
+    public Practicant(string name)
+    {
+        Name = name;
+    }
+    public void Teach()
+    {
+        Console.WriteLine("I am a student");
+    }
+}
--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,11 +1,16 @@
 ﻿WorkWith(new MathTeacher("Alice"));
 WorkWith(new PhysicsTeacher("John"));
-
-
+WorkWith(new Practicant("Tom"));
+WorkWith("");
 
 void WorkWith(object obj)
 {
+    // dynamic teacher = obj; // String not contain definition for Name, WorkWith("");
+    // var teacher = (ITeacher)obj; // unable to cast Practicant to ITeacher, WorkWith(new Practicant("Tom"));
     var teacher = obj as ITeacher;
-    Console.WriteLine(teacher?.Name);
-    teacher?.Teach();
+    if (teacher!=null)
+    {
+        Console.WriteLine(teacher?.Name);
+        teacher?.Teach();
+    }
 }
